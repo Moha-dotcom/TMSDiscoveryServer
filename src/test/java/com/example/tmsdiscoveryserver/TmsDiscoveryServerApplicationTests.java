6f55322855c6218adaf67f037453785eeb5d3ecb
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-10-17 12:38:50 -0500

    discovery server for TMS Project

---
+++ src/test/java/com/example/tmsdiscoveryserver/TmsDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmsdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
